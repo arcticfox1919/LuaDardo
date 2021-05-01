458ea465e090f60d57cbec8afe48af99ad9b6e80
arcticfox <flutterdev@88.com>
2021-05-02 06:28:59 +0800

    Modify the directory structure

--- lib/lua.dart
+++ lib/lua.dart
@@ -1,14 +1,12 @@
-library lua_dardo;
+import 'src/state/lua_state_impl.dart';
 
-import 'state/lua_state_impl.dart';
+import 'src/api/lua_state.dart';
 
-import 'api/lua_state.dart';
-
-export 'package:lua_dardo/api/lua_state.dart';
-export 'package:lua_dardo/api/lua_basic_api.dart';
-export 'package:lua_dardo/api/lua_aux_lib.dart';
-export 'package:lua_dardo/api/lua_vm.dart';
-export 'package:lua_dardo/api/lua_type.dart';
+export 'src/api/lua_state.dart';
+export 'src/api/lua_basic_api.dart';
+export 'src/api/lua_aux_lib.dart';
+export 'src/api/lua_vm.dart';
+export 'src/api/lua_type.dart';
 
 
 
