89f53bc8debcdf367393e4ecf3aa64e051528724
mytrin <martin.lepeska@gmail.com>
2022-12-30 21:03:59 +0100

    Table traversing test

--- test/stdlib/table_test.dart
+++ test/stdlib/table_test.dart
@@ -32,8 +32,47 @@ print(table.concat(a1, ","))
   return true;
 }
 
+bool testTableTraversing(){
+  bool result = false;
+
+  try{
+    LuaState state = LuaState.newState();
+    state.openLibs();
+    state.loadString(r'''
+local alphabet = {}
+alphabet.a = 'alpha'
+alphabet.b = 'beta'
+
+--trigger keys initializastion
+local val = next(alphabet, 'b')
+
+alphabet.c = 'gamma'
+
+result = false;
+for k, v in pairs(alphabet) do
+  if (k == 'c' and v == 'gamma')
+  then
+    result = true;
+  end
+end
+''');
+    state.pCall(0, 0, 1);
+    LuaType resultType = state.getGlobal("result");
+    if (resultType == LuaType.luaBoolean) {
+      result = state.toBoolean(state.getTop());
+    }
+  } catch(e,s){
+    print('$e\n$s');
+  }
+
+  return result;
+}
+
 void main() {
-  test('lua table standard library test', () {
-    expect(testTable(), true);
+  //test('lua table standard library test', () {
+  //  expect(testTable(), true);
+  //});
+  test('\nlua traversing table test', () {
+    expect(testTableTraversing(), true);
   });
 }
